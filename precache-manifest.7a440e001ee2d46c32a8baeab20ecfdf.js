4093654df6a0b868bfe58b8d878f4c433aec3012
Cyber_pineapple <cyber_pineapple@mail.ru>
2019-06-24 12:49:32 +0300

    Updates

---
+++ precache-manifest.7a440e001ee2d46c32a8baeab20ecfdf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f25cef91517fe8db0c0e047b0a2978d7",
+    "url": "/snp-todo-react/index.html"
+  },
+  {
+    "revision": "d23a4dfedacf20c0c70c",
+    "url": "/snp-todo-react/static/css/main.07f208cf.chunk.css"
+  },
+  {
+    "revision": "dd08e16bccacb581f0fc",
+    "url": "/snp-todo-react/static/js/2.65b0b8df.chunk.js"
+  },
+  {
+    "revision": "d23a4dfedacf20c0c70c",
+    "url": "/snp-todo-react/static/js/main.d72e6fec.chunk.js"
+  },
+  {
+    "revision": "71642cd0d2369696a0c9",
+    "url": "/snp-todo-react/static/js/runtime~main.d72860e6.js"
+  },
+  {
+    "revision": "cf001d50bb051b62263e42281cd675f4",
+    "url": "/snp-todo-react/static/media/8bit-vremya-sutki-noch-more-2339.cf001d50.jpg"
+  },
+  {
+    "revision": "f1422591b9cde9c8e2ae1f1a1ca476e1",
+    "url": "/snp-todo-react/static/media/Dited.f1422591.otf"
+  },
+  {
+    "revision": "b7a6d8a701ee76bb63eaae200fde3f54",
+    "url": "/snp-todo-react/static/media/da3bd826876f76c.b7a6d8a7.png"
+  }
+]);
